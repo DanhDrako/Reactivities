9e588b8994d6064b917b44ecac149eaed473b6d6
Danh Doan <nkocdanh58@gmail.com>
2025-02-15 16:33:59 +0700

    Initial code

--- Persistence/Migrations/20250215092815_AddInitial.Designer.cs
+++ Persistence/Migrations/20250215092815_AddInitial.Designer.cs
@@ -12,15 +12,15 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20250214101619_InitialCreate1")]
-    partial class InitialCreate1
+    [Migration("20250215092815_AddInitial")]
+    partial class AddInitial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "9.0.2")
+                .HasAnnotation("ProductVersion", "9.0.1")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
